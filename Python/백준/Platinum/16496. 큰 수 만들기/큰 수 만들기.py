46209292e69d1832064cc7aeae550d15b12e12ef
lyhtll <166387914+lyhtll@users.noreply.github.com>
2024-06-13 17:23:54 +0900

    [Platinum V] Title: 큰 수 만들기, Time: 864 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.py
@@ -0,0 +1,13 @@
+n=int(input())
+li=input().split()
+if (sum(map(int,li)) == 0):
+    print(0)
+else:
+
+    t=''
+    for i in li:
+        for j in range(len(li)-1):
+            if int(li[j]+li[j+1])<int(li[j+1]+li[j]):
+                li[j],li[j+1]=li[j+1],li[j]
+    print(*li,sep='')
+
